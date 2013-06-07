a538728d8e0defd35937c165cf22f458c837895b
Blair Anderson <blair81@gmail.com>
2013-06-07 14:42:16 -0600

    working on the portoflio

---
+++ portfolios/anderson_blair.rb
@@ -0,0 +1,34 @@
+
+{
+  :last_name => "Anderson",
+  :first_name => "Blair",
+  :email_address => "blair81@gmail.com",
+  :looking_for => "I'm looking for a role in a team, where learning and writing good code are a top priority.",
+  :best_at => ["RSpec TDD with Ruby/Rails", 'Understanding a problem/business process and converting it to software', 'Getting Things Done!'],
+  :looking_for => ["Up to three bullet points of what you're looking for", 'Bullet two', "Bullet three"],
+  :locations => ["Boulder", "Denver", "San Francisco", "Portland", "San Diego"]
+  :code => {
+    :github => 'https://github.com/blairanderson',
+    :project_1 => {
+      :title => "ShareMore"
+      :description => "ShareMore was fun because I had decided to tackle a simple -problem but make sure it was fully completed. In 7-days I created a main application and API structure, a small client library to interact with the application, and experimented with an Ember application as a front-end."
+      :url => 'https://github.com/blairanderson/share-more'
+      :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
+    }
+    :project_2 => {
+      :title => "Title as you want it to appear"
+      :description => "1-3 sentences about why this is interesting"
+      :url => 'http://github.com/yourusername/yourproject'
+      :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
+    }
+    :project_3 => {
+      :title => "Title as you want it to appear"
+      :description => "1-3 sentences about why this is interesting"
+      :url => 'http://github.com/yourusername/yourproject'
+      :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
+    }
+  },
+  :resume => "http://blairbuilds.herokuapp.com/resume.html",
+  :references => ['']
+  :other_notes => "I am Awesome."
+}
