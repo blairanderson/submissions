594276e82efa58828a9de9043631706c386d009e
Blair Anderson <blair81@gmail.com>
2013-06-07 19:28:43 -0600

    WIP

--- portfolios/anderson_blair.rb
+++ portfolios/anderson_blair.rb
@@ -10,10 +10,10 @@
   :code => {
     :github => 'https://github.com/blairanderson',
     :project_1 => {
-      :title => "ShareMore"
-      :description => "ShareMore was fun because I had decided to tackle a simple -problem but make sure it was fully completed. In 7-days I created a main application and API structure, a small client library to interact with the application, and experimented with an Ember application as a front-end."
+      :title => "ShareMore - A Twitter client without a character limit."
+      :description => "ShareMore was fun because I had decided to tackle a simple problem but make sure it was fully completed. In 7-days I created a main application and API structure, a small client library to interact with the application, and experimented with an Ember application as a front-end."
       :url => 'https://github.com/blairanderson/share-more'
-      :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
+      :what_to_look_for => ['HyperMedia API end at /api/v1', 'The Simplicity of The Gem', 'Clean Design and Thorough Implementation']
     }
     :project_2 => {
       :title => "Title as you want it to appear"
@@ -22,9 +22,9 @@
       :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
     }
     :project_3 => {
-      :title => "Title as you want it to appear"
-      :description => "1-3 sentences about why this is interesting"
-      :url => 'http://github.com/yourusername/yourproject'
+      :title => "HaxHub - Integrating the APIs of Github, Pivotal Tracker, and Travis CI to serve as an all-in-one software project feed."
+      :description => "This was a fun experience trying to create a single-page application that aggregated a external content in real-time. We integrated WebHooks as well as some basic jQuery/CoffeeScript poling for new data."
+      :url => 'https://github.com/blairanderson/haxhub'
       :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
     }
   },
