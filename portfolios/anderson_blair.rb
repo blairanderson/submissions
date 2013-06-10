1d3fd3a2cea2d6b20e4b256052d706713b7022ae
Blair Anderson <blair81@gmail.com>
2013-06-10 15:17:29 -0600

    completing my portfolio

--- portfolios/anderson_blair.rb
+++ portfolios/anderson_blair.rb
@@ -1,34 +1,33 @@
-
 {
   :last_name => "Anderson",
   :first_name => "Blair",
   :email_address => "blair81@gmail.com",
-  :looking_for => "I'm looking for a role in a team, where learning and writing good code are a top priority.",
-  :best_at => ["RSpec TDD with Ruby/Rails", 'Understanding a problem/business process and converting it to software', 'Getting Things Done!'],
-  :looking_for => ["Up to three bullet points of what you're looking for", 'Bullet two', "Bullet three"],
-  :locations => ["Boulder", "Denver", "San Francisco", "Portland", "San Diego"]
+  :looking_for_description => "I don't _just_ want to join a company, I want to join a team. I want to work with a group of people that care about educating each-other and working towards a common goal. I'm most passionate about disrupting politics, healthcare, payments, and education but would be happy to work outside of these industries. I'm eager to pair with amazing programmers, and to continually become better.",
+  :best_at => ['Testing First', 'Writing Clean Code', 'Getting Things Done!'],
+  :looking_for => ["Continuous Learning", 'Building Products', "Having Fun"],
+  :locations => ["Boulder", "Denver", "San Francisco", "Portland", "New York"],
   :code => {
     :github => 'https://github.com/blairanderson',
     :project_1 => {
       :title => "ShareMore - A Twitter client without a character limit."
       :description => "ShareMore was fun because I had decided to tackle a simple problem but make sure it was fully completed. In 7-days I created a main application and API structure, a small client library to interact with the application, and experimented with an Ember application as a front-end."
       :url => 'https://github.com/blairanderson/share-more'
-      :what_to_look_for => ['HyperMedia API end at /api/v1', 'The Simplicity of The Gem', 'Clean Design and Thorough Implementation']
+      :what_to_look_for => ['HyperMedia API endpoint at /api/v1', 'The Simplicity of The Gem', 'Clean Design and Thorough Implementation']
     }
     :project_2 => {
-      :title => "Title as you want it to appear"
-      :description => "1-3 sentences about why this is interesting"
-      :url => 'http://github.com/yourusername/yourproject'
-      :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
+      :title => "RunTalk - a mobile chat system"
+      :description => "RunTalk is a mobile chat system. It uses services to break up the architecture into more manageable pieces."
+      :url => 'http://github.com/blairanderson/runtalk'
+      :what_to_look_for => ['You get services!', 'and you get services!', 'and you get services!']
     }
     :project_3 => {
       :title => "HaxHub - Integrating the APIs of Github, Pivotal Tracker, and Travis CI to serve as an all-in-one software project feed."
-      :description => "This was a fun experience trying to create a single-page application that aggregated a external content in real-time. We integrated WebHooks as well as some basic jQuery/CoffeeScript poling for new data."
+      :description => "This was a fun experience trying to create a single-page application that aggregated a external content in real-time. We integrated WebHooks as well as some basic jQuery/CoffeeScript polling for new data."
       :url => 'https://github.com/blairanderson/haxhub'
-      :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
+      :what_to_look_for => ['Consuming data through webhook', 'Good extraction of data extraction through APIs', 'Clean Implementation of jQuery for polling']
     }
   },
   :resume => "http://blairbuilds.herokuapp.com/resume.html",
   :references => ['']
-  :other_notes => "I am Awesome."
+  :other_notes => "I am Awesome. Find me on twitter @blairanderson"
 }
